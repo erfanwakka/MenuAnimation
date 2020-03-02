9708927286319abd70d814dc3f5d7c0601070c4d
erfan andesta <andesta.erfan@gmail.com>
2020-03-02 13:00:17 +0330

    Initial Commit

---
+++ MenuAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuAnimation
+//
+//  Created by erfan on 12/12/1398 AP.
+//  Copyright © 1398 Erfan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
